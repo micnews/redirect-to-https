64aa58fb7620619f3016c37a359115783d910ead
David Björklund <david.bjorklund@gmail.com>
2014-12-29 16:13:10 -0500

    working implementation

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var httpToHttps = function (req, res) {
+      if (!req.headers['x-forwarded-proto']) return false
+      if (req.headers['x-forwarded-proto'] === 'https') return false
 
+      var host = req.headers['x-forwarded-host'] || req.headers.host
+
+      res.setHeader('location', 'https://' + host + req.url)
+      res.statusCode = 301
+      res.end()
     }
 
 module.exports = httpToHttps
--- test.js
+++ test.js
@@ -12,7 +12,9 @@ var http = require('http')
     })
 
   , setupServer = function (headers, callback) {
-      servertest(server, '/', { encoding: 'utf8', headers: headers }, callback)
+      var options = { encoding: 'utf8', headers: headers }
+
+      servertest(server, '/hello?foo=bar', options, callback)
     }
 
 test('no x-forwarded-proto header', function (t) {
@@ -21,6 +23,46 @@ test('no x-forwarded-proto header', function (t) {
 
     t.equal(res.statusCode, 200)
     t.equal(res.body, 'beep boop')
+    t.end()
+  })
+})
+
+test('x-forwarded-proto=https', function (t) {
+  setupServer({ 'x-forwarded-proto': 'https' }, function (err, res) {
+    if (err) return t.end(err)
+
+    t.equal(res.statusCode, 200)
+    t.equal(res.body, 'beep boop')
+    t.end()
+  })
+})
+
+test('x-forwarded-proto=http', function (t) {
+  var headers = {
+        'x-forwarded-proto': 'http'
+      }
+
+  setupServer(headers, function (err, res) {
+    if (err) return t.end(err)
+
+    t.equal(res.statusCode, 301)
+    t.ok(/^https:\/\/localhost:\d+\/hello\?foo=bar$/.test(res.headers.location))
+    t.end()
+  })
+})
+
+test('x-forwarded-proto=http, x-forwarded-host=example.com', function (t) {
+  var headers = {
+          'x-forwarded-proto': 'http'
+        , 'x-forwarded-host': 'example.com'
+      }
+
+  setupServer(headers, function (err, res) {
+    if (err) return t.end(err)
+
+    t.equal(res.statusCode, 301)
+    t.equal(res.headers.location, 'https://example.com/hello?foo=bar')
+
     t.end()
   })
 })
